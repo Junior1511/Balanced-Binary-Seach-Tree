014ced9773061bd9843ee95881791b709ded1a25
Junior1511 <yunior122000@gmail.com>
2026-03-10 20:17:47 -0500

    fixed the #build_tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,49 +1,32 @@
-# require_relative "node"
+require_relative "node"
 
 class Tree
-
-  class Node
-    attr_accessor :data, :left, :right
-
-    def initialize(data = nil, left = nil, right = nil)
-      @data = data
-      @left = left
-      @right = right
-    end
-  end
 
   attr_accessor :root
 
   def initialize(arr)
-    @root = nil
-    build_tree(arr)
+    @root = build_tree(arr)
   end
 
   
   # private_class_method :build_tree
 
   def build_tree(arr)
-    return nil if arr.length < 3
     clean_arr = arr.uniq.sort
-    p "clean array #{clean_arr}"
-    mid_index = (clean_arr.length / 2.0).round - 1
-    mid = clean_arr[mid_index]
-    p "mid value #{mid} , mid index #{mid_index}"
-    left = clean_arr[0...mid_index]
-    right = clean_arr[mid_index+1..-1]
-    @root = Node.new(mid, Tree.new(left), Tree.new(right))
-    return @root
+    return nil if clean_arr.empty?
+    mid_index = clean_arr.length / 2
+    mid_value = clean_arr[mid_index]
+    remainder_left = clean_arr[0...mid_index]
+    remainder_right = clean_arr[mid_index+1..-1]
+    return Node.new(mid_value,build_tree(remainder_left),build_tree(remainder_right))
   end
 
   def pretty_print(node = @root, prefix = '', is_left: true)
-  return unless node
+    return unless node
 
-  pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", is_left: false)
-  puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-  pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", is_left: true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", is_left: false)
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", is_left: true)
   end
 
 end
-
-t = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-p t
---
+++ main.rb
@@ -0,0 +1,4 @@
+require_relative "lib/tree"
+
+t = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+t.pretty_print
